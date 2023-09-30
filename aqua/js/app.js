7615d73f64a203bbcaf808738b6423871a034b86
Mir512mir <123588015+Mir512mir@users.noreply.github.com>
2023-09-30 14:21:00 +0200

    Add files via upload

---
+++ aqua/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.reviews__carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+    });
+});
+console.log();
